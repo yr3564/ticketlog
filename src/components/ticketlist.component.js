322c97027a02204a533a96bb6864379cacb31d4c
YamilRuiz <yr3564@att.com>
2019-10-01 19:50:07 -0500

    Added create-ticket component

---
+++ src/components/ticketlist.component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class TicketList extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Ticket List</h3>
+                <table>
+                    
+                </table>
+            </div>
+        )
+    }
+}
