9ced6f27520ba3eafa56009a3827d4bd0ab324cf
YamilRuiz <yr3564@att.com>
2019-10-14 20:33:11 -0500

    sites file and route get and post added

--- backend/routes/sites.js
+++ backend/routes/sites.js
@@ -1,5 +1,26 @@
 const router= require('express').Router();
-
+let Site= require('../models/sites.model')
 router.route('/').get((req,res)=>{
-    
+    Site.find()
+    .then(sites=>res.json(sites))
+    .catch(err=>res.status(400).json("Found Error "+err));
+})
+
+router.route('/add').post((req,res)=>{
+    const site= req.body.site;
+    const lat= req.body.lat;
+    const lng= req.body.lng;
+    const hidden=false;
+    const date= Date.parse(req.body.date)
+
+    const newSite= new Site({
+        site,
+        lat,
+        lng,
+        hidden,
+        date
+    })
+    newSite.save()
+    .then(()=>res.json("Site Added!"))
+    .catch(err=>res.status(400).json("Error has occured "= err))
 })
